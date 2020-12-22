71648b52f5172d3d28021360b98585f1676ca012
Ekaterinafieryglow <71172000+Ekaterinafieryglow@users.noreply.github.com>
2020-12-22 23:48:54 +0500

    Лабораторная работа 13
    Добавление кода программы

---
+++ practice4/ConsoleApplication6/ConsoleApplication6/ConsoleApplication6.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+#include <ctime>
+#include <random>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	int N;
+	int i = 1;
+	int number = 0;
+	string cry;
+	vector <int> a;
+
+	setlocale(LC_ALL, "RUS");
+
+	cout << "Введите количество бочек в мешке: ";
+	cin >> N;
+
+	while (i <= N) 
+	{
+		cout << "Нажмите любую клавишу, чтобы вытянуть бочонок: ";
+		cin >> cry;
+		do 
+		{
+			number = rand() % N;
+		} while (find(a.begin(), a.end(), number) != a.end());
+		a.push_back(number);
+		cout << "Число на бочонке: " << number + 1 << endl;
+		i++;
+	}
+	cout << "Мешок пустой! Все бочонки были вытащены";
+}
